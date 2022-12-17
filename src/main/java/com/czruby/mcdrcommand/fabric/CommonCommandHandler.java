f88cc0a18dd4750213127f4ebb77dfe95ea06478
czruby <3116878993@qq.com>
2022-12-17 16:37:04 +0800

    1.19.2

---
+++ src/main/java/com/czruby/mcdrcommand/fabric/CommonCommandHandler.java
@@ -0,0 +1,18 @@
+package com.czruby.mcdrcommand.fabric;
+
+import org.apache.logging.log4j.LogManager;
+import net.minecraft.server.command.ServerCommandSource;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+
+public class CommonCommandHandler implements Command<ServerCommandSource> {
+    @Override
+    public int run(CommandContext<ServerCommandSource> context) {
+        LogManager.getLogger().info(
+                "<{}> {}",
+                context.getSource().getName(),
+                context.getInput()
+        );
+        return Command.SINGLE_SUCCESS;
+    }
+}
---
+++ src/main/java/com/czruby/mcdrcommand/fabric/MCDRCommand.java
@@ -0,0 +1,25 @@
+package com.czruby.mcdrcommand.fabric;
+
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import net.fabricmc.api.ModInitializer;
+
+public class MCDRCommand implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register(
+                (dispatcher, registryAccess, environment) -> dispatcher.register(
+                        CommandManager.literal("mcdr")
+                                .requires(source -> source.getEntity() == null)
+                                .then(
+                                        CommandManager.literal("register")
+                                                .then(
+                                                        CommandManager.argument("data", StringArgumentType.greedyString())
+                                                                .executes(new RegisterCommandHandler())
+                                                )
+                                )
+                )
+        );
+    }
+}
